161643bf4494a1b6c6f0d536e149a2585d459081
aRusenov <bobpodesta@yahoo.in>
2015-06-18 13:30:17 +0300

    added solid logger skeleton

---
+++ 09. SolidLogger-Skeleton/SOLIDLogger/Logger.cs
@@ -0,0 +1,37 @@
+﻿namespace SOLIDLogger
+{
+    using System;
+
+    public class Logger
+    {
+        public void Info(string msg)
+        {
+            var date = DateTime.Now;
+            Console.WriteLine("{0} - {1} - {2}", date, ReportLevel.Info, msg);
+        }
+
+        public void Warn(string msg)
+        {
+            var date = DateTime.Now;
+            Console.WriteLine("{0} - {1} - {2}", date, ReportLevel.Info, msg);
+        }
+
+        public void Error(string msg)
+        {
+            var date = DateTime.Now;
+            Console.WriteLine("{0} - {1} - {2}", date, ReportLevel.Info, msg);
+        }
+
+        public void Critical(string msg)
+        {
+            var date = DateTime.Now;
+            Console.WriteLine("{0} - {1} - {2}", date, ReportLevel.Info, msg);
+        }
+
+        public void Fatal(string msg)
+        {
+            var date = DateTime.Now;
+            Console.WriteLine("{0} - {1} - {2}", date, ReportLevel.Info, msg);
+        }
+    }
+}
---
+++ 09. SolidLogger-Skeleton/SOLIDLogger/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SOLIDLogger
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Logger logger = new Logger();
+            logger.Critical("Out of memory");
+            logger.Info("Unused local variable 'name'");
+        }
+    }
+}
